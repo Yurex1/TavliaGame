58827f6a382c351ad187cea33446097663c4a7b2
Yurex <popovych.yuriy2@gmail.com>
2023-10-14 20:32:40 +0300

    + create user

--- backend/dist/src/app.module.js
+++ backend/dist/src/app.module.js
@@ -10,14 +10,15 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const auth_module_1 = require("./auth/auth.module");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
+        imports: [auth_module_1.AuthModule],
     })
 ], AppModule);
 //# sourceMappingURL=app.module.js.map
