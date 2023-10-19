b1cb66df4415d0bc6b739c5d3c84fc2b93188463
Roma <toftlegokl@gmail.com>
2023-10-19 12:00:52 +0300

    Create SideBarLogItem

---
+++ frontend/components/SideBar/SideBarLogItem.tsx
@@ -0,0 +1,17 @@
+const SideBareLogItem = () => {
+  if (true) {
+    return (
+      <div className="aside-item">
+        <div className="text">Log In</div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="aside-item">
+        <div className="text">Log Out</div>
+      </div>
+    );
+  }
+};
+
+export default SideBareLogItem;
