457a2cb85fdbccc77ad03ca4100af170964a9914
Roma <toftlegokl@gmail.com>
2023-10-19 11:49:50 +0300

    Create SideBar and SideBarItem

---
+++ frontend/components/SideBar/index.tsx
@@ -0,0 +1,24 @@
+import SideBarItem from "./SideBarItem";
+export default function SideBare() {
+  return (
+    <aside>
+      <div className="top-items">
+        <a href="../" className="name">
+          {/* <div className='img'></div> */}
+          <div className="text">
+            <b>Tavlia</b>
+          </div>
+        </a>
+        <SideBarItem text="Play" hrefText="../" />
+        <SideBarItem text="Instraction" hrefText="../" />
+        <SideBarItem text="Profile" hrefText="../" />
+        <SideBarItem text="Rank" hrefText="../" />
+        <SideBarItem text="Friends" hrefText="../" />
+        <SideBarItem text="Settings" hrefText="../" />
+      </div>
+      <div className="down-items">
+        <SideBarItem text="Help" hrefText="../" />
+      </div>
+    </aside>
+  );
+}
