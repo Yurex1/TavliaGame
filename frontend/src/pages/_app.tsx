a81896bc487de431799d849cd2b7822f54d7409d
Roma <toftlegokl@gmail.com>
2023-10-24 21:23:36 +0300

    Add Auth to site

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
+import Auth from "@components/Auth";
 import SideBare from "@components/SideBar";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      {true &&  < Auth />}
       <SideBare />
       <div className="main">
         <Component  {...pageProps} />
