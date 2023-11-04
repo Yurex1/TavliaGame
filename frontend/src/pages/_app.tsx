a51dffda1114fc87670ed4b61c0cc27e4d3dcdc1
Roma <toftlegokl@gmail.com>
2023-11-04 20:34:37 +0200

    add AuthClick setLoggedIn and setShowAuth

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,15 +1,20 @@
 import "@/styles/globals.css";
-import Auth from "@components/Auth";
-import SideBare from "@components/SideBar";
+import Auth from "@/components/Auth";
+import SideBare from "@/components/SideBar";
 import type { AppProps } from "next/app";
 import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [showAuth, setShowAuth] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+  function AuthClick() {
+    setLoggedIn(true);
+    setShowAuth(false);
+  }
   return (
     <>
-      {showAuth &&  < Auth />}
-      <SideBare setShowAuth={setShowAuth}/>
+      {showAuth &&  < Auth AuthClick = {AuthClick}/>}
+      <SideBare setLoggedIn = {setLoggedIn} loggedIn = {loggedIn} setShowAuth={setShowAuth}/>
       <div className="main">
         <Component  {...pageProps} />
       </div>
