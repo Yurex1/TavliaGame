fbbaa21442009184e37de158cf65ca1ba7f77670
Roma <toftlegokl@gmail.com>
2023-11-07 14:42:22 +0200

    add query to app

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,6 +3,9 @@ import Auth from "@/components/Auth";
 import SideBare from "@/components/SideBar";
 import type { AppProps } from "next/app";
 import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   const [showAuth, setShowAuth] = useState(false);
@@ -13,11 +16,13 @@ export default function App({ Component, pageProps }: AppProps) {
   }
   return (
     <>
+     <QueryClientProvider client={queryClient}>
       {showAuth &&  < Auth AuthClick = {AuthClick}/>}
       <SideBare setLoggedIn = {setLoggedIn} loggedIn = {loggedIn} setShowAuth={setShowAuth}/>
       <div className="main">
         <Component  {...pageProps} />
       </div>
+      </QueryClientProvider>
     </>
   );
 }
