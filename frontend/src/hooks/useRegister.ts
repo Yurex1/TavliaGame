da34d206b2814ffd5a619885becbcd244a93aa35
Roma <toftlegokl@gmail.com>
2023-11-07 14:42:42 +0200

    add hook useRegister

---
+++ frontend/src/hooks/useRegister.ts
@@ -0,0 +1,18 @@
+import { User } from "@/types/User";
+import axios from "axios";
+import { useMutation, useQueryClient } from "react-query";
+
+const register = async (user: User) => {
+  return axios.post("/auth", user);
+};
+
+const useRegister = () => {
+  const queryClient = useQueryClient();
+  return useMutation(register, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["user"]);
+    },
+  });
+};
+
+export default useRegister;
