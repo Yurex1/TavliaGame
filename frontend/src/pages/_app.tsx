9e2d608fcacf159dbb1d433544a7583d024c951c
Roma <toftlegokl@gmail.com>
2023-11-09 19:50:18 +0200

    remove authClick

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,21 +4,17 @@ import SideBare from "@/components/SideBar";
 import type { AppProps } from "next/app";
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import useUser from "@/hooks/useUser";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   const [showAuth, setShowAuth] = useState(false);
-  const [loggedIn, setLoggedIn] = useState(false);
-  function AuthClick() {
-    setLoggedIn(true);
-    setShowAuth(false);
-  }
   return (
     <>
      <QueryClientProvider client={queryClient}>
-      {showAuth &&  < Auth AuthClick = {AuthClick}/>}
-      <SideBare setLoggedIn = {setLoggedIn} loggedIn = {loggedIn} setShowAuth={setShowAuth}/>
+      {showAuth &&  < Auth setShowAuth = {setShowAuth}/>}
+      <SideBare setShowAuth={setShowAuth}/>
       <div className="main">
         <Component  {...pageProps} />
       </div>
