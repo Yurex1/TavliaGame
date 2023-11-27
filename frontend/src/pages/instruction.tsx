ca25b56c049b75ee2bfae5c068df427fb31e49a1
Roma <toftlegokl@gmail.com>
2023-11-27 17:45:57 +0200

    page instruction

---
+++ frontend/src/pages/instruction.tsx
@@ -0,0 +1,19 @@
+
+export default function Instruction() {
+    return (
+      <>
+        <div className="instruction">
+            <div className="wrapper">
+                <div>
+                    <h1>Instruction</h1>
+                    <p>1. Choose the size of the board you want to play on.</p>
+                    <p>2. Click on the board to place your pieces.</p>
+                    <p>3. The first player to get 5 pieces in a row wins.</p>
+                    ....
+                </div>
+            </div>
+        </div>
+      </>
+    );
+  }
+  
