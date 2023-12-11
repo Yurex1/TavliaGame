d6376bc3876afb9406e7e357c8d7a98e21a95bfa
Roma <toftlegokl@gmail.com>
2023-12-11 22:08:22 +0200

    change profile, add list of friend and games

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -6,6 +6,113 @@ export default function Profile() {
   const username = "tedi20";
   const avatar = "Avatar.png";
   const rang = "2100";
+
+  const friendList = [
+    {
+      id: "1",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "2",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "3",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "4",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "5",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "6",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "7",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "8",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "9",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "10",
+      name: "tedi20",
+      rank: 120,
+    },
+  ];
+
+  const gameList = [
+    {
+      id: "1",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "2",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "3",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "4",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "5",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "6",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "7",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "8",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "9",
+      name: "tedi20",
+      rank: 120,
+    },
+    {
+      id: "10",
+      name: "tedi20",
+      rank: 120,
+    },
+  ];
+
   return (
     <>
       <div className="profile">
@@ -17,18 +124,36 @@ export default function Profile() {
               <div className="rang">rating: {rang}</div>
             </div>
           </div>
-          <select className="select" size = {2}>
-          <option>asd</option>
-          <option>asd</option>
-          <option>asd</option><option>asd</option><option>asd</option><option>asd</option><option>asd</option><option>asd</option><option>asd</option><option>asd</option><option>asd</option><option>asd</option>
-          <option>asd</option>
-          <option>asd</option>
-          <option>asd</option>
-          <option>asd</option>
-          <option>asd</option>
-          <option>asd</option>
-          <option>asd</option>
-          </select>
+          <div className="lists">
+            <div className="full-list">
+              <div className="list-name">Friend</div>
+              <div className="list">
+                {friendList.map((item) => {
+                  return (
+                    <div key={item.id} className="list-item">
+                      <div className="id">{item.id}</div>
+                      <div className="user-name">{item.name}</div>
+                      <div className="user-rank">{item.rank}</div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="full-list">
+              <div className="list-name">Game</div>
+              <div className="list">
+                {gameList.map((item) => {
+                  return (
+                    <div key={item.id} className="list-item">
+                      <div className="id">{item.id}</div>
+                      <div className="user-name">{item.name}</div>
+                      <div className="user-rank">{item.rank}</div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
