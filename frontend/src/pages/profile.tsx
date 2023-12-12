65c008e7036a0b77cb0f1a3e751dc55b75a092d4
Roma <toftlegokl@gmail.com>
2023-12-12 15:15:20 +0200

    change profile page

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import useUser from "../hooks/useUser";
+import FriendListItem from "@/components/ListItem/FriendListItem";
+import GameListItem from "@/components/ListItem/GameListItem";
 
 export default function Profile() {
   const user = useUser();
@@ -11,105 +13,80 @@ export default function Profile() {
     {
       id: "1",
       name: "tedi20",
-      rank: 120,
+      rating: 120,
     },
     {
       id: "2",
       name: "tedi20",
-      rank: 120,
+      rating: 120,
     },
     {
       id: "3",
       name: "tedi20",
-      rank: 120,
+      rating: 120,
     },
     {
       id: "4",
       name: "tedi20",
-      rank: 120,
+      rating: 120,
     },
     {
       id: "5",
       name: "tedi20",
-      rank: 120,
+      rating: 120,
     },
     {
       id: "6",
       name: "tedi20",
-      rank: 120,
+      rating: 120,
     },
     {
       id: "7",
       name: "tedi20",
-      rank: 120,
+      rating: 120,
     },
     {
       id: "8",
       name: "tedi20",
-      rank: 120,
+      rating: 120,
     },
     {
       id: "9",
       name: "tedi20",
-      rank: 120,
+      rating: 120,
     },
     {
       id: "10",
       name: "tedi20",
-      rank: 120,
+      rating: 120,
     },
   ];
 
   const gameList = [
     {
       id: "1",
-      name: "tedi20",
-      rank: 120,
+      color: "king",
+      status: "win",
     },
     {
-      id: "2",
-      name: "tedi20",
-      rank: 120,
+      id: "1",
+      color: "king",
+      status: "lose",
     },
     {
-      id: "3",
-      name: "tedi20",
-      rank: 120,
+      id: "1",
+      color: "king",
+      status: "win",
     },
     {
-      id: "4",
-      name: "tedi20",
-      rank: 120,
+      id: "1",
+      color: "king",
+      status: "lose",
     },
     {
-      id: "5",
-      name: "tedi20",
-      rank: 120,
-    },
-    {
-      id: "6",
-      name: "tedi20",
-      rank: 120,
-    },
-    {
-      id: "7",
-      name: "tedi20",
-      rank: 120,
-    },
-    {
-      id: "8",
-      name: "tedi20",
-      rank: 120,
-    },
-    {
-      id: "9",
-      name: "tedi20",
-      rank: 120,
-    },
-    {
-      id: "10",
-      name: "tedi20",
-      rank: 120,
+      id: "1",
+      color: "king",
+      status: "lose",
     },
   ];
 
@@ -128,29 +105,17 @@ export default function Profile() {
             <div className="full-list">
               <div className="list-name">Friend</div>
               <div className="list">
-                {friendList.map((item) => {
-                  return (
-                    <div key={item.id} className="list-item">
-                      <div className="id">{item.id}</div>
-                      <div className="user-name">{item.name}</div>
-                      <div className="user-rank">{item.rank}</div>
-                    </div>
-                  );
-                })}
+                {friendList.map((item) => (
+                  <FriendListItem {...item} />
+                ))}
               </div>
             </div>
             <div className="full-list">
               <div className="list-name">Game</div>
               <div className="list">
-                {gameList.map((item) => {
-                  return (
-                    <div key={item.id} className="list-item">
-                      <div className="id">{item.id}</div>
-                      <div className="user-name">{item.name}</div>
-                      <div className="user-rank">{item.rank}</div>
-                    </div>
-                  );
-                })}
+                {gameList.map((item) => 
+                    <GameListItem {...item} />  
+              )}
               </div>
             </div>
           </div>
