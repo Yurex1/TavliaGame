f223e94ebbf5db3fec5767a174a126d71a38e94e
Yurex1 <popovych.yuriy2@gmail.com>
2023-12-13 18:40:37 +0200

    +add game logic and full game process

--- backend/src/events/events.gateway.ts
+++ backend/src/events/events.gateway.ts
@@ -16,6 +16,11 @@ import { io } from "socket.io-client";
 import { PrismaService } from "src/prisma.service";
 import { Room } from "src/chess/room/room";
 
+interface Move {
+  from: { x: number, y: number },
+  to: { x: number, y: number },
+}
+
 @WebSocketGateway({
   namespace: "events",
 })
@@ -27,8 +32,11 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @WebSocketServer() io: Namespace;
   private logger = new Logger();
-  private gameMoves: string[] = [];
+  private gameMoves: Move[] = [];
   private rooms: Map<string, Room> = new Map();
+  private firstUserId: number | null = null;
+  private secondUserId: number | null = null;
+  private isGameAlreadyInDatabase: boolean = false;
 
 
   RandomRoom = (): string => {
@@ -46,18 +54,28 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   }
 
   @SubscribeMessage("createRoom")
-  createRoom(socket: Socket) {
+  createRoom(socket: Socket, data: { n: number, userId: number }) {
+
+    if (data.n === null || data.n === undefined || data.userId === null || data.userId === undefined) {
+      socket.emit('status', 'one of values is null or undefined');
+      return;
+    }
+    this.firstUserId = data.userId;
     const roomId: string = this.RandomRoom();
     socket.join(roomId);
+    this.rooms.set(roomId, new Room(socket.id, data.n));
 
-    this.rooms.set(roomId, new Room(roomId, socket.id));
-    socket.emit('getRoomId', roomId)
+    socket.emit('getRoomId', roomId);
     // this.io.emit("createNewGame", { roomId: roomId });
   }
 
   @SubscribeMessage("joinRoom")
-  joinRoom(socket: Socket, data: { roomId }) {
-    console.log("Room: ", data.roomId)
+  joinRoom(socket: Socket, data: { roomId: string, userId: number }) {
+    if (data.roomId === null || data.roomId === undefined || data.userId === null || data.userId === undefined) {
+      socket.emit('status', 'one of values is null or undefined');
+      return;
+    }
+    this.secondUserId = data.userId;
     const room = this.rooms.get(data.roomId);
     if (!room) {
       socket.emit("status", "No room with this roomId");
@@ -77,13 +95,58 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   }
 
   @SubscribeMessage("move")
-  handleMove(socket: Socket, data: { move }) {
-    console.log()
-    const room = this.rooms.get(socket.rooms[0]);
+  async handleMove(socket: Socket, data: { moveFrom: { x: number, y: number }, moveTo: { x: number, y: number } }) {
+    let roomId;
+    if (Array.from(socket.rooms)[0] === socket.id) {
+      roomId = Array.from(socket.rooms)[1];
+    } else {
+      roomId = Array.from(socket.rooms)[0];
+    }
+    const room = this.rooms.get(roomId);
     if (room) {
-      socket.broadcast.to(socket.rooms[0]).emit("move", data.move);
-      room.makeMove(data.move);
-      // this.io.to(data.roomId).emit("move", data.move);
+      const move = room.makeMove(data.moveFrom, data.moveTo);
+
+      if (move === "Success") {
+
+        this.gameMoves.push({
+          from: data.moveFrom,
+          to: data.moveTo
+        })
+        console.log("Moves: ", this.gameMoves)
+        this.io.to(roomId).emit("move", { from: data.moveFrom, to: data.moveTo });
+      }
+      else if (move === "Game is over" && !this.isGameAlreadyInDatabase) {
+        this.gameMoves.push({
+          from: data.moveFrom,
+          to: data.moveTo
+        })
+        this.isGameAlreadyInDatabase = true;
+        await this.prismaService.game.create({
+          data:
+          {
+
+            users: {
+              connect: [
+                { id: this.firstUserId },
+                { id: this.secondUserId }
+              ]
+            },
+            Move: {
+              create: this.gameMoves.map(move => ({
+                fromX: move.from.x,
+                fromY: move.from.y,
+                toX: move.to.x,
+                toY: move.to.y
+              })),
+            },
+          },
+
+        }).then((res) => console.log("Ok", res)).catch(res => console.log("Error prisma", res));
+        this.io.to(roomId).emit("move", "End of the game");
+      }
+      else {
+        this.io.to(roomId).emit("move", "Incorrect move");
+      }
     } else {
       this.io.emit("move status", "No room with this roomId");
       throw new NotFoundException();
@@ -92,13 +155,6 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @SubscribeMessage("end game")
   async handleEndOfTheGame(data: { roomId; player1; player2 }) {
-    if (
-      data.player1 === undefined ||
-      data.player2 === undefined ||
-      data.roomId === undefined
-    ) {
-      return "Error. Data is undefined";
-    }
     this.io.socketsLeave(data.roomId);
     const user1 = await this.prismaService.user.findUnique({
       where: { login: data.player1 },
@@ -107,15 +163,17 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
       where: { login: data.player2 },
     });
 
-    return await this.prismaService.game.create({
-      data: {
-        moves: this.gameMoves,
-        users: { connect: [{ id: user1.id }, { id: user2.id }] },
-      },
-    });
+    // return await this.prismaService.game.create({
+    //   data: {
+    //     moves: this.gameMoves,
+    //     users: { connect: [{ id: user1.id }, { id: user2.id }] },
+    //   },
+    // });
   }
 
   handleDisconnect(client: Socket) {
+
+    this.io.to(this.rooms[client.id]).emit('status', "Client disconnected: ", client.id)
     console.log("Event disconnect ");
   }
 }
