3451805c65a1b6a2227e27fff2ca79277ee1290a
Roma <toftlegokl@gmail.com>
2023-12-25 23:46:03 +0200

    change game component

--- frontend/src/components/GameTavlia.tsx
+++ frontend/src/components/GameTavlia.tsx
@@ -1,27 +1,28 @@
 import { useEffect, useState } from "react";
 import BoardComponent from "./BoardComponent";
-import { Board } from "../models/Board";
+import { Board, Status } from "../models/Board";
+import { Game } from "../models/Game";
 import Table from "./Table";
+import { set } from "react-hook-form";
+import { Colors } from "@/models/Colors";
 
 export default function GameTavlia({n} : any) {
-    const[board, setBoard] = useState(new Board(n));
+    const[game, setGame] = useState(new Game(n));
+    const[status, setStatus] = useState(Status.PLAYING);
+    const[history, setHistory] = useState(game.history);
+    const[move, setMove] = useState(Colors.WHITE);
     useEffect(() => {
         restart();
     },[n]);
 
-    useEffect(() => {
-        restart();
-    }, []);
-
     function restart(){
-        const newBoard = new Board(n);
-        newBoard.addFigures();
-        setBoard(newBoard);
+        setGame(new Game(n));
     }
+
     return (
         < div className = "game">
-            <BoardComponent board={board} setBoard={setBoard}/>
-            <Table />
+            <BoardComponent game={game} setMove = {setMove} setHistory = {setHistory} setStatus = {setStatus}/>
+            <Table status = {status} move = {move} history={history}/>
         </div>
     );
 }
