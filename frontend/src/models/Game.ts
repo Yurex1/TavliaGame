34caa160b63b2b4e7dda959f00f39904d340c261
Roma <toftlegokl@gmail.com>
2023-12-25 23:43:46 +0200

    rewrite game logic

---
+++ frontend/src/models/Game.ts
@@ -0,0 +1,32 @@
+import { Board, Status } from "./Board";
+import { Colors } from "./Colors";
+import { Square } from "./Square";
+import { Figure } from "./figures/Figure";
+
+export type Move = {
+  from: Square;
+  to: Square;
+};
+
+export class Game {
+  readonly n: number;
+  color: Colors;
+  history: Move[] = [];
+  king: Figure | null = null;
+  status = Status.PLAYING;
+  board: Board;
+  constructor(n: number) {
+    this.n = n;
+    this.color = Colors.WHITE;
+    this.board = new Board(n, this);
+    if (n)
+      this.king =
+        this.board.squares[(this.n - 1) / 2][(this.n - 1) / 2].figure!;
+  }
+
+  public changeColor() {
+    this.color == Colors.WHITE
+      ? (this.color = Colors.BLACK)
+      : (this.color = Colors.WHITE);
+  }
+}
