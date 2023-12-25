63e5fc8483623ed225f56e8cb2738a36909afa7e
Roma <toftlegokl@gmail.com>
2023-12-25 23:40:32 +0200

    add game2x2

---
+++ frontend/src/pages/game2x2.tsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import GameTavlia from "../components/GameTavlia";
+
+export default function Game2x2() {
+  const router = useRouter();
+  const { n } = router.query;
+  return (
+    <>
+      <div className='wrapper'>
+        <GameTavlia n = {n}/>
+      </div>
+    </>
+  );
+}
