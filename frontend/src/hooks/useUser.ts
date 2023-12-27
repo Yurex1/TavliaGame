994e2c9054385eb1eb29e758e7f8689d0d83d4f2
Roma <toftlegokl@gmail.com>
2023-12-28 00:19:18 +0200

    final useUser

--- frontend/src/hooks/useUser.ts
+++ frontend/src/hooks/useUser.ts
@@ -4,7 +4,7 @@ import { useQuery } from "react-query";
 
 const getUser = async () => {
     if(localStorage.getItem('token') === null) return null;
-    return await axios.get(API_URL + 'auth/profile' , {headers: {Authorization: "Bearer" + localStorage.getItem('token')?.toString()}});
+    return await axios.get(API_URL + 'auth/profile' , {headers: {Authorization: "Bearer " + localStorage.getItem('token')?.toString()}});
 }
 
 const useUser= () => {
