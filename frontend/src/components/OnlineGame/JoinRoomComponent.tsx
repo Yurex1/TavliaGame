c4d40b437aba6441795572acb31f31ac32a92984
Roma <toftlegokl@gmail.com>
2023-12-29 18:09:48 +0200

    join

--- frontend/src/components/OnlineGame/JoinRoomComponent.tsx
+++ frontend/src/components/OnlineGame/JoinRoomComponent.tsx
@@ -14,7 +14,7 @@ type JoinRoomComponentProps = {
 
 const JoinRoomComponent: FC<JoinRoomComponentProps> = ({setRoomId, userId, setStatus, setColor}) => {
     const methods = useForm();
-    const joinRoom = (data: any) => {
+    const joinRoom = (data: {roomId: string}) => {
         SocketApi.socket?.emit("joinRoom", { roomId: data.roomId, userId });
         SocketApi.socket?.on("status", (statusMessage: string) => {
             if(statusMessage === "start game"){
