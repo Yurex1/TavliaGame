05a63d6f1d7a4a208021354901ba32864887d781
Roma <toftlegokl@gmail.com>
2023-12-29 12:12:49 +0200

    jpin room

---
+++ frontend/src/components/OnlineGame/JoinRoomComponent.tsx
@@ -0,0 +1,39 @@
+import React, { FC} from "react";
+import RHFInput from "../Form/RHFinput";
+import Form from "../Form";
+import { useForm } from "react-hook-form";
+import SocketApi from "@/api/socket-api";
+import { Colors } from "@/models/Colors";
+
+type JoinRoomComponentProps = {
+    setStatus: (status: string) => void;
+    userId: number;
+    setRoomId: (roomId: string) => void;
+    setColor: (color: string) => void;
+};
+
+const JoinRoomComponent: FC<JoinRoomComponentProps> = ({setRoomId, userId, setStatus, setColor}) => {
+    const methods = useForm();
+    const joinRoom = (data: any) => {
+        SocketApi.socket?.emit("joinRoom", { roomId: data.roomId, userId });
+        SocketApi.socket?.on("status", (statusMessage: string) => {
+            if(statusMessage === "start game"){
+                setRoomId(data.roomId);
+                setColor(Colors.BLACK);
+                setStatus(statusMessage);
+            }
+            else{
+                alert(statusMessage);
+            }
+        });
+    }
+  return (
+    <div className="cen">
+      <Form methods={methods} submitText="Join" onSubmit={joinRoom}>
+        <RHFInput placeholder="room id" type="text" name="roomId"/>
+      </Form>
+    </div>
+  );
+};
+
+export default JoinRoomComponent;
