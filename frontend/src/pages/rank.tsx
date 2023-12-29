9410e8e3e1f58d9ddb9cd379ef3b01030b90e309
anarchist1312 <21.serg.l@gmail.com>
2023-12-29 17:31:38 +0200

    sorted rank list

--- frontend/src/pages/rank.tsx
+++ frontend/src/pages/rank.tsx
@@ -1,7 +1,19 @@
+import API_URL from "@/constants";
+import axios from "axios";
 import { useState } from "react";
+import { useQuery } from "react-query";
 
 export default function Rank() {
-  const list = [
+
+  const getUser = async () => {
+    return await axios.get(API_URL + 'auth/profile', {withCredentials: true});
+}
+
+  const useUser= () => {
+    return useQuery(['user'], getUser);
+};
+
+  const initialList = [
     {
       id: "1",
       name: "tedi20",
@@ -9,55 +21,67 @@ export default function Rank() {
     },
     {
       id: "2",
-      name: "tedi20",
-      rank: 120,
+      name: "john",
+      rank: 90,
     },
     {
       id: "3",
-      name: "tedi20",
-      rank: 120,
+      name: "alice",
+      rank: 110,
     },
     {
       id: "4",
-      name: "tedi20",
-      rank: 120,
-    },
-    {
-      id: "5",
-      name: "tedi20",
-      rank: 120,
-    },
-    {
-      id: "6",
-      name: "tedi20",
-      rank: 120,
-    },
-    {
-      id: "7",
-      name: "tedi20",
-      rank: 120,
+      name: "bobaaaaaaaaaaaaaaa",
+      rank: 200,
     },
   ];
-  const [friend , setFriend] = useState(false);
-  
+
+  const [list, setList] = useState([...initialList]);
+  const [sortOrder, setSortOrder] = useState({ field: 'rank', order: 'asc' });
+  const [friend, setFriend] = useState(false);
+
+  const sortList = (field:'id'|'rank'|'name') => {
+    const order = sortOrder.field === field && sortOrder.order === 'asc' ? 'desc' : 'asc';
+
+    const sortedList = [...list].sort((a, b) => {
+      if (field === 'rank') {
+        return order === 'asc' ? a.rank - b.rank : b.rank - a.rank;
+      } else {
+        return order === 'asc' ? a[field].localeCompare(b[field]) : b[field].localeCompare(a[field]);
+      } 
+      return 0;
+    });
+
+    setList(sortedList);
+    setSortOrder({ field, order });
+  };
+
+  const renderList = friend ? list.filter(item => false) : list;
+
   return (
     <>
       <div className="rank">
         <div className="wrapper">
           <div className="rank-buttons">
-            <button onClick = {() => setFriend(false)} className="rank-button">Rank</button>
-            <button onClick = {() => setFriend(true)} className="rank-button">Friend rank</button>
+            <button onClick={() => sortList('id')} className="rank-button id">
+              ID
+            </button>
+            <button onClick={() => sortList('name')} className="rank-button user-name">
+              Name
+            </button>
+            <button onClick={() => sortList('rank')} className="rank-button">
+              Rank
+            </button>
+            
           </div>
           <ul className="rank-standing">
-            {list.map((item) => {
-              return (
-                <li key={item.id} className="rank-item">
-                  <div className="id">{item.id}</div>
-                  <div className="user-name">{item.name}</div>
-                  <div className="user-rank">{item.rank}</div>
-                </li>
-              );
-            })}
+            {renderList.map((item) => (
+              <li key={item.id} className="rank-item">
+                <div className="id">{item.id}</div>
+                <div className="user-name">{item.name}</div>
+                <div className="user-rank">{item.rank}</div>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
