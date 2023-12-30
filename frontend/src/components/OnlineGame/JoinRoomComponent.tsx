bc32ce84be8f1b0236af4ec551650c481d7de0eb
Roma <toftlegokl@gmail.com>
2023-12-30 18:15:48 +0200

    change full online game

--- frontend/src/components/OnlineGame/JoinRoomComponent.tsx
+++ frontend/src/components/OnlineGame/JoinRoomComponent.tsx
@@ -2,34 +2,18 @@ import React, { FC } from "react";
 import RHFInput from "../Form/RHFinput";
 import Form from "../Form";
 import { useForm } from "react-hook-form";
-import SocketApi from "@/api/socket-api";
-import { Colors } from "@/models/Colors";
+import { SocketApiType } from "@/hooks/useConnect";
 
 type JoinRoomComponentProps = {
-  setStatus: (status: string) => void;
-  userId: number;
-  setRoomId: (roomId: string) => void;
-  setColor: (color: string) => void;
+  socket: SocketApiType;
 };
 
 const JoinRoomComponent: FC<JoinRoomComponentProps> = ({
-  setRoomId,
-  userId,
-  setStatus,
-  setColor,
+  socket
 }) => {
   const methods = useForm();
   const joinRoom = (data: { roomId: string }) => {
-    SocketApi.socket?.emit("joinRoom", { roomId: data.roomId, userId });
-    SocketApi.socket?.on("status", (statusMessage: string) => {
-      if (statusMessage === "start game") {
-        setRoomId(data.roomId);
-        setColor(Colors.BLACK);
-        setStatus(statusMessage);
-      } else {
-        alert(statusMessage);
-      }
-    });
+    socket.joinRoom(data.roomId);
   };
   return (
     <div className="cen">
--- frontend/src/components/OnlineGame/index.tsx
+++ frontend/src/components/OnlineGame/index.tsx
@@ -4,21 +4,15 @@ import OnlineTavliaGame from "./OnlineTavliaGame";
 import CreateRoomComponent from "./CreateRoomComponenet";
 import { Colors } from "@/models/Colors";
 import JoinRoomComponent from "./JoinRoomComponent";
-import { Game } from "@/models/Game";
 import MainMenu from "./MainMenu";
 
 type OnlineGameProps = {
   n: number;
-  userId: number;
-  username: string;
 };
 
-const OnlineGame: FC<OnlineGameProps> = ({ n, userId}) => {
-  useConnect();
-  const [roomId, setRoomId] = React.useState<string | null>(null);
+const OnlineGame: FC<OnlineGameProps> = ({n}) => {
+  const socket = useConnect(n);
   const [status, setStatus] = React.useState<string | null>(null);
-  const [color, setColor] = React.useState<string>(Colors.EMPTY);
-  const [game, setGame] = React.useState<Game>(new Game(n));
 
   if (status === null)
     return (
@@ -27,22 +21,17 @@ const OnlineGame: FC<OnlineGameProps> = ({ n, userId}) => {
   if (status === "create") {
     return (
       <CreateRoomComponent
-        n={n}
-        setRoomId={setRoomId}
-        setStatus={setStatus}
-        roomId={roomId}
-        userId={userId}
-        setColor={setColor}
+        socket={socket}
       />
     );
   }
   if(status === "join"){
     return (
-        <JoinRoomComponent userId={userId} setColor={setColor} setRoomId={setRoomId} setStatus={setStatus}/>
+        <JoinRoomComponent socket = {socket} />
     );
   }
   if (status === "start game") {
-    return <OnlineTavliaGame game = {game} setGame = {setGame} roomId = {roomId} userId = {userId} n = {n} color={color}/>;
+    return <OnlineTavliaGame socket = {socket}/>;
   }
 };
 
