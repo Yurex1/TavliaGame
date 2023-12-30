fb4ddbb1564a954a624c30bd71d1b20f7af4e723
anarchist1312 <21.serg.l@gmail.com>
2023-12-31 00:04:18 +0200

    justified rank, small fixes(doesnt work)

--- frontend/src/pages/rank.tsx
+++ frontend/src/pages/rank.tsx
@@ -5,35 +5,12 @@ import { useState } from "react";
 export default function Rank() {
 
   const usersList = useRank();
-
-  const initialList = [
-    {
-      id: "1",
-      name: "tedi20",
-      rank: 120,
-    },
-    {
-      id: "2",
-      name: "john",
-      rank: 90,
-    },
-    {
-      id: "3",
-      name: "alice",
-      rank: 110,
-    },
-    {
-      id: "4",
-      name: "bobaaaaaaaaaaaaaaa",
-      rank: 200,
-    },
-  ];
-
-  const [list, setList] = useState([...initialList]);
+//@ts-ignore
+  const [list, setList] = useState([...usersList.data]);
   const [sortOrder, setSortOrder] = useState({ field: 'rank', order: 'asc' });
   const [friend, setFriend] = useState(false);
 
-  const sortList = (field:'id'|'rank'|'name') => {
+  const sortList = (field:'rank'|'name') => {
     const order = sortOrder.field === field && sortOrder.order === 'asc' ? 'desc' : 'asc';
 
     const sortedList = [...list].sort((a, b) => {
@@ -56,23 +33,19 @@ export default function Rank() {
       <div className="rank">
         <div className="wrapper">
           <div className="rank-buttons">
-            <button onClick={() => sortList('id')} className="rank-button id">
-              ID
-            </button>
-            <button onClick={() => sortList('name')} className="rank-button user-name">
-              Name
-            </button>
             <button onClick={() => sortList('rank')} className="rank-button">
               Rank
+            </button>
+            <button onClick={() => sortList('name')} className="rank-button">
+              Name
             </button>
             
           </div>
           <ul className="rank-standing">
             {renderList.map((item) => (
               <li key={item.id} className="rank-item">
-                <div className="id">{item.id}</div>
-                <div className="user-name">{item.name}</div>
                 <div className="user-rank">{item.rank}</div>
+                <div className="user-name">{item.name}</div>
               </li>
             ))}
           </ul>
