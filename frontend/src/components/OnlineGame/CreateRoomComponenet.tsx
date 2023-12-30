a96c690ee631531ac77ea5d9293aceba1b4d040f
Roma <toftlegokl@gmail.com>
2023-12-31 00:27:31 +0200

    hange full socket

--- frontend/src/components/OnlineGame/CreateRoomComponenet.tsx
+++ frontend/src/components/OnlineGame/CreateRoomComponenet.tsx
@@ -1,5 +1,6 @@
+import SocketApi from "@/api/socket-api";
 import { SocketApiType } from "@/hooks/useConnect";
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 
 type CreateRoomProps = {
   socket: SocketApiType;
@@ -8,15 +9,17 @@ type CreateRoomProps = {
 const CreateRoomComponent: FC<CreateRoomProps> = ({
   socket,
 }) => {
+  const [roomId, setRoomId] = React.useState<string | null>(null);
+
 
   return (
     <div className="cen">
       <div className="auth">
-        <div onClick={socket.createGame} className="btn">
+        <div onClick={() => {socket.createGame(setRoomId)}} className="btn">
           {" "}
           Get roomId
         </div>
-        <input className="" defaultValue={socket.roomId ? socket.roomId : ""} />
+        <input className="" defaultValue={roomId ? roomId : ""} />
       </div>
     </div>
   );
--- frontend/src/components/OnlineGame/OnlineTavliaGame.tsx
+++ frontend/src/components/OnlineGame/OnlineTavliaGame.tsx
@@ -1,37 +1,56 @@
 import { Status } from "@/models/Board";
-import { Colors } from "@/models/Colors";
 import { Game, Move } from "@/models/Game";
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import SquareComponent from "../GameTavlia/SquareComponent";
 import { Square } from "@/models/Square";
 import SocketApi from "@/api/socket-api";
-import TableOnline from "./TableOnline";
 import { SocketApiType } from "@/hooks/useConnect";
 
 type OnlineTavliaGameProps = {
   socket: SocketApiType;
+  history: Move[];
+  userId: number;
+  moverId: number | null;
+  moveStatus: string;
 };
 
-const OnlineTavliaGame: FC<OnlineTavliaGameProps> = ({socket }) => {
-  const [game, ] = useState(new Game(socket.n!, socket.history));
+const OnlineTavliaGame: FC<OnlineTavliaGameProps> = ({
+  socket,
+  history,
+  userId,
+  moverId,
+}) => {
+  const [game, setGame] = useState(new Game(socket.n!, socket.history));
   const [selectedSquare, setSelectedSquare] = useState<Square | null>(null);
   function click(square: Square) {
-    if (socket.moverId != socket.userId && socket.moveStatus == "can move" && game.status==Status.PLAYING) return;
-    if (selectedSquare && selectedSquare == square) {
-      game.board.highlightSquares(null);
-      setSelectedSquare(null);
-      return;
-    }
-    if (selectedSquare && selectedSquare.figure?.canMove(square)) {
-      const move : Move = {from: {x: selectedSquare.x, y: selectedSquare.y}, to: {x: square.x, y: square.y}};
-      SocketApi.move(move);
-      return;
-    }
-    if (square.figure && square.figure.color == game.color) {
-      game.board.highlightSquares(square);
-      setSelectedSquare(square);
+    if (
+      moverId == userId &&
+      socket.moveStatus == "can move" &&
+      game.status == Status.PLAYING
+    ) {
+      if (selectedSquare && selectedSquare == square) {
+        game.board.highlightSquares(null);
+        setSelectedSquare(null);
+        return;
+      }
+      if (selectedSquare && selectedSquare.figure?.canMove(square)) {
+        const move: Move = {
+          from: { x: selectedSquare.x, y: selectedSquare.y },
+          to: { x: square.x, y: square.y },
+        };
+        SocketApi.move(move, setSelectedSquare);
+        return;
+      }
+      if (square.figure && square.figure.color == game.color) {
+        game.board.highlightSquares(square);
+        setSelectedSquare(square);
+      }
     }
   }
+
+  useEffect(() => {
+    setGame(new Game(socket.n!, history));
+  }, [history.length]);
 
   return (
     <>
@@ -53,9 +72,9 @@ const OnlineTavliaGame: FC<OnlineTavliaGameProps> = ({socket }) => {
             </React.Fragment>
           ))}
         </div>
-        <TableOnline
+        {/* <TableOnline
           socket={socket}
-        />
+        /> */}
       </div>
     </>
   );
