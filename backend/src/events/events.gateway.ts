2aa4174bca95c6413d117525f73774f77026d3b0
Yurex1 <popovych.yuriy2@gmail.com>
2024-01-02 03:23:29 +0200

    change emit name to 'winner'

--- backend/src/events/events.gateway.ts
+++ backend/src/events/events.gateway.ts
@@ -221,7 +221,7 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
       room.saveGame();
       this.io.to(roomId).emit("move", { from: data.moveFrom, to: data.moveTo });
       this.io.to(roomId).emit("game status", "End of the game");
-      this.io.to(roomId).emit("game status", `winner ${room.whoWin()}`);
+      this.io.to(roomId).emit("winner", `${room.whoWin()}`);
       this.io.socketsLeave(roomId);
       this.rooms.delete(roomId)
       this.players.delete(room.player1);
@@ -258,7 +258,7 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
       return;
     }
 
-    this.io.to(roomId).emit("game status", `winner, ${room.whoWin()}`);
+    this.io.to(roomId).emit("surrender", `${room.whoWin()}`);
     this.io.socketsLeave(roomId);
     this.rooms.delete(roomId)
     this.players.delete(room.player1);
@@ -287,16 +287,14 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
       }
       else if (result === '0 players left') {
         if (roomPlayer1) {
-
           this.players.delete(roomPlayer1);
         }
         if (roomPlayer2) {
-
           this.players.delete(roomPlayer2);
         }
         room.surrender(room.firstLogout)
         this.io.to(roomId).emit("game status", `Player ${room.firstLogout} surrendered`);
-        this.io.to(roomId).emit("game status", `winner ${room.player1 === room.firstLogout ? room?.player2 : room?.player1}`);
+        this.io.to(roomId).emit("winner", `${room.player1 === room.firstLogout ? room?.player2 : room?.player1}`);
         this.io.socketsLeave(roomId);
         this.rooms.delete(roomId)
       }
