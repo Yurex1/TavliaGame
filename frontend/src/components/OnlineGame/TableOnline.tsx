6d044695da91c3c3ed9a172702d0e560b20d842f
Roma <toftlegokl@gmail.com>
2024-01-02 12:44:05 +0200

    detect winer

--- frontend/src/components/OnlineGame/TableOnline.tsx
+++ frontend/src/components/OnlineGame/TableOnline.tsx
@@ -73,7 +73,7 @@ const Table: FC<TableProps> = ({ socket, gameStatus, moverId, winerId }) => {
     <div className="table">
      
         {winerId ? (
-          <div className="table-status">{winerId == socket.whiteId ? <>{white} win!</> : <>{black} win!</>} {gameStatus=="Player surrendered" && (winerId == socket.whiteId ? <>{black} surrendered!</> : <>{white} surrendered!</>)}</div>
+          <div className="table-status">{winerId == socket.whiteId ? <>{white} win!</> : <>{black} win!</>} {gameStatus!="End of the game" && (winerId == socket.whiteId ? <>{black} surrendered!</> : <>{white} surrendered!</>)}</div>
         ) : (
           <div className="table-status">{mover} moving</div>
         )}
