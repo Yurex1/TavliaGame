fad7f74094806364f799ddfe6ce6e8a9fbfdcaf5
Roma <toftlegokl@gmail.com>
2024-01-02 15:51:46 +0200

    login

--- frontend/src/components/Auth/Singup.tsx
+++ frontend/src/components/Auth/Singup.tsx
@@ -15,6 +15,7 @@ const Singup: FC<SingupProps> = ({ setShowAuth }) => {
   const methods = useForm();
   const { mutateAsync: singup } = useRegister({setError, setShowAuth});
   const onSubmit = async (data: User) => {
+    data.name = data.login;
     if(data.name === "" || data.email === "" || data.password === "" || data.login === ""){
       setError("All fields must be filled");
       return;
@@ -24,7 +25,6 @@ const Singup: FC<SingupProps> = ({ setShowAuth }) => {
   return (
     <Form methods={methods} submitText="Sign in" onSubmit={onSubmit}>
       <RHFInput placeholder="Login" name="login" type="text" />
-      <RHFInput placeholder="Name" name="name" type="text" />
       <RHFInput placeholder="Email" name="email" type="email" />
       <RHFInput placeholder="Password" name="password" type="password" />
       {error && <div className="error">{error}</div>}
