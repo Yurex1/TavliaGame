1421a672ac95fa3d901d42eb44ab0a047e3ae608
Roma <toftlegokl@gmail.com>
2024-01-05 00:43:45 +0200

    add ukraine and english dto

---
+++ frontend/src/pages/instruction/instructionData/UkraineDTO.ts
@@ -0,0 +1,25 @@
+import InstructionDTO from "./instructionDTO";
+
+const UkraineDTO = () => {
+  return {
+    Title: "Інструкція",
+    FirstSubTitle: "Основні Відомості про Гру",
+    FirstSubTitleFirstLable:
+      "Тавлія - одна з найдавніших настільних ігор, яка походить з Скандинавії. На жаль, точні правила гри не збереглися до наших часів, тому ми використовуватимемо одні з найбільш ймовірних.",
+    FirstSubTitleSecondLable: "Для гри використовуються різні розміри дошки: 7x7, 9x9 та 11x11.",
+    FirstSubTitleThirdLable: "Незалежно від розміру дошки, правила гри залишаються незмінними.",
+    SecondSubTitle: "Правила гри",
+    SecondSubTitleFirstLable: "Гра є імітацією того, як варвари підстерігли короля з його захисниками.",
+    SecondSubTitleSecondLable: "Гра розпочинається з короля.",
+    SecondSubTitleThirdLable: "Короля розміщують у центрі дошки, а навколо нього його лицарі.",
+    SecondSubTitleFourthLable: "Варвари розташовані з чотирьох сторін дошки, кількість варварів удвічі більше, ніж лицарів.",     
+    SecondSubTitleFifthLable: `<b>Всі</b> фігури рухаються горизонтально чи вертикально на будь-яку кількість вільних клітинок.`,
+    SecondSubTitleSixthLable: "Для захоплення фігури супротивника, її слід оточити <b>вашим</b> ходами з двох протилежних сторін.",
+    SecondSubTitleSeventhLable: "Король перемагає, якщо він вибирається з засідки, дійшовши до однієї з 4 кутових клітин.",
+    SecondSubTitleEighthLable: "Ніхто, крім короля, не може займати кутові клітини.",
+    SecondSubTitleNinthLable: "Варвари перемагають, оточивши короля з усіх 4 сторін.",
+    SecondSubTitleTenthLable: "Неможливість зробити хід - це поразка.", 
+} as InstructionDTO;
+};
+
+export default UkraineDTO;
