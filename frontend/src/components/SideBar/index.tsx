a10a96e3c69efb81803fbd532b233136472d1cb4
Roma <toftlegokl@gmail.com>
2024-01-04 21:55:45 +0200

    add language change on side bar

--- frontend/src/components/SideBar/index.tsx
+++ frontend/src/components/SideBar/index.tsx
@@ -1,37 +1,83 @@
-import {  FC,  } from "react";
+import { FC, useEffect, useState } from "react";
 import SideBarItem from "./SideBarItem";
 import SideBarLogItem from "./SideBarLogItem";
+import React from "react";
+
 type SideBareProps = {
   setShowAuth: (showAuth: boolean) => void;
-}
-import React from 'react'
+};
 
+type SideBarConfig = {
+  text: string;
+  hrefText: string;
+  img_url: string;
+};
 
-const SideBare:FC<SideBareProps> = ({setShowAuth}) => {
+const sideBarConfig: SideBarConfig[] = [
+  {
+    text: "Play",
+    hrefText: "../",
+    img_url: "Lightning.png",
+  },
+  {
+    text: "Rank",
+    hrefText: "../rank",
+    img_url: "rank.png",
+  },
+  {
+    text: "Instruction",
+    hrefText: "../instruction",
+    img_url: "info.png",
+  },
+];
+
+const SideBare: FC<SideBareProps> = ({ setShowAuth }) => {
+  const [language , setLanguage] = useState<string>("En");
+  useEffect(() => {
+    const tempLanguage = localStorage.getItem("language");
+    if(tempLanguage){
+      setLanguage(tempLanguage);
+    }
+  },[]);
+
+  const change = () => {
+    if(language == "En")
+    {
+      localStorage.setItem("language", "Ua");
+    }
+    else{
+      localStorage.setItem("language", "En");
+    }
+    const tempLanguage = localStorage.getItem("language");
+    if(tempLanguage){
+      setLanguage(tempLanguage);
+    }
+  }
+
   return (
     <aside>
       <div className="top-items">
         <a href="../" className="name">
-          {/* <div className='img'></div> */}
           <div>
             <b>T</b>
             <b className="notformobile">avlia</b>
           </div>
         </a>
-
-        <SideBarItem text="Play" hrefText="../" img_url={"Lightning.png"}  />
-        {/* <SideBarItem text="Profile" hrefText="../" img_url={"prof.png"} /> */}
-        <SideBarItem text="Rank" hrefText="../rank" img_url={"rank.png"} />
-        <SideBarItem text="Instruction" hrefText="../instruction" img_url={"info.png"} />
-        {/* <SideBarItem text="Friends" hrefText="../" img_url={"friends.png"} /> */}
-        {/* <SideBarItem text="Settings" hrefText="../" img_url={"sett.png"} /> */}
-        <SideBarLogItem setShowAuth = {setShowAuth}/>
+        {sideBarConfig.map((item, index) => (
+          <SideBarItem
+            key={index}
+            text={item.text}
+            hrefText={item.hrefText}
+            img_url={item.img_url}
+          />
+        ))}
+        <SideBarLogItem setShowAuth={setShowAuth} />
       </div>
-      {/* <div className="down-items">
-        <SideBarItem text="Help" hrefText="../" img_url={"help.png"} />
-      </div> */}
+      <div className = "language center" onClick={change}>
+        <div className="language-text">language:</div>{language}
+      </div>
     </aside>
   );
-}
+};
 
 export default SideBare;
