140192d1c8ca1cbc87d6f2a74984182e79cc4c8b
Roma <toftlegokl@gmail.com>
2024-01-06 21:18:00 +0200

    dropdown ua

---
+++ frontend/src/Data/DropDown/EnglishData.ts
@@ -0,0 +1,8 @@
+import DropDownData from "./DropDownData";
+
+export const EnglishData = {
+  Play: "Play",
+  PlayWithBot: "Play with bot",
+  PlayOnOneDesk: "Play on one desk",
+  PlayOnline: "Play online",
+} as DropDownData;
