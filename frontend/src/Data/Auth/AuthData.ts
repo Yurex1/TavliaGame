abf4613b8f927750037ef64eb1b9f8527911ef06
Roma <toftlegokl@gmail.com>
2024-01-06 21:31:32 +0200

    auth ua

---
+++ frontend/src/Data/Auth/AuthData.ts
@@ -0,0 +1,11 @@
+type DropDownData = {
+    Name: string;
+    Email: string;
+    Password: string;
+    Register: string;
+    Login: string;
+    AlreadyHaveAnAccount: string;
+    DontHaveAnAccount: string;
+}
+
+export default DropDownData;
--- frontend/src/components/Auth/Singup.tsx
+++ frontend/src/components/Auth/Singup.tsx
@@ -5,6 +5,7 @@ import RHFInput from "../Form/RHFinput";
 import useRegister from "@/hooks/useRegister";
 import React from "react";
 import { User } from "@/types/types";
+import authData from "@/Data/Auth";
 
 type SingupProps = {
   setShowAuth: (showAuth: boolean) => void;
@@ -13,20 +14,26 @@ type SingupProps = {
 const Singup: FC<SingupProps> = ({ setShowAuth }) => {
   const [error, setError] = React.useState<string>("");
   const methods = useForm();
-  const { mutateAsync: singup } = useRegister({setError, setShowAuth});
+  const { mutateAsync: singup } = useRegister({ setError, setShowAuth });
   const onSubmit = async (data: User) => {
     data.name = data.login;
-    if(data.name === "" || data.email === "" || data.password === "" || data.login === ""){
+    if (
+      data.name === "" ||
+      data.email === "" ||
+      data.password === "" ||
+      data.login === ""
+    ) {
       setError("All fields must be filled");
       return;
     }
     await singup(data);
   };
+  const DTO = authData();
   return (
-    <Form methods={methods} submitText="Sign in" onSubmit={onSubmit}>
-      <RHFInput placeholder="Login" name="login" type="text" />
-      <RHFInput placeholder="Email" name="email" type="email" />
-      <RHFInput placeholder="Password" name="password" type="password" />
+    <Form methods={methods} submitText={DTO.Register} onSubmit={onSubmit}>
+      <RHFInput placeholder={DTO.Name} name="login" type="text" />
+      <RHFInput placeholder={DTO.Email} name="email" type="email" />
+      <RHFInput placeholder={DTO.Password} name="password" type="password" />
       {error && <div className="error">{error}</div>}
     </Form>
   );
