901b98217d43d9c3eb880ffe399ec88e563f5a0e
Roma <toftlegokl@gmail.com>
2024-01-06 21:17:15 +0200

    сайт бар українська

---
+++ frontend/src/Data/SideBar/UkraineData.ts
@@ -0,0 +1,24 @@
+import SideBarData from "./SideBarData";
+
+export const UkraineData = {
+  Title: "Тавлія",
+  Config: [
+    {
+      text: "Грати",
+      hrefText: "../",
+      img_url: "Lightning.png",
+    },
+    {
+      text: "Ранг",
+      hrefText: "../rank",
+      img_url: "rank.png",
+    },
+    {
+      text: "Інструкція",
+      hrefText: "../instruction",
+      img_url: "info.png",
+    },
+  ],
+  Login: "Увійти",
+  Logout: "Вийти",
+} as SideBarData;
