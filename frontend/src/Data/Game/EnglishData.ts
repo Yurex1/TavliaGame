38931156bf990a8df20d56f93ab0bc6ff18d977c
Roma <toftlegokl@gmail.com>
2024-01-06 21:58:24 +0200

    translate game

---
+++ frontend/src/Data/Game/EnglishData.ts
@@ -0,0 +1,9 @@
+import GameData from "./GameData";
+
+export const EnglishData = {
+  GetRoomId: "Get game id",
+  RoomId: "Game id",
+  Back: "Back",
+  CreateRoom: "Create game",
+  JoinRoom: "Join game",
+} as GameData;
