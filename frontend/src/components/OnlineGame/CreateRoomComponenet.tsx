6e912bae7970378ceb6b25374c87e6239cc07b20
Roma <toftlegokl@gmail.com>
2024-01-06 21:58:24 +0200

    translate game

--- frontend/src/components/OnlineGame/CreateRoomComponenet.tsx
+++ frontend/src/components/OnlineGame/CreateRoomComponenet.tsx
@@ -1,27 +1,34 @@
+import gameData from "@/Data/Game";
 import { SocketApiType } from "@/hooks/useConnect";
-import React, { FC  } from "react";
+import React, { FC } from "react";
 
 type CreateRoomProps = {
   socket: SocketApiType;
   restart: () => void;
 };
 
-const CreateRoomComponent: FC<CreateRoomProps> = ({
-  socket,
-  restart,
-}) => {
+const CreateRoomComponent: FC<CreateRoomProps> = ({ socket, restart }) => {
   const [roomId, setRoomId] = React.useState<string | null>(null);
-
+  const DTO = gameData();
 
   return (
     <div className="main-menu center column">
-
-        <div onClick={() => {socket.createGame(setRoomId)}} className="form-button center">
-          {" "}
-          Get roomId
-        </div>
-        <input className="" defaultValue={roomId ? roomId : ""} />
-        <div onClick={restart} className="form-button center">Back</div>
+      <div
+        onClick={() => {
+          socket.createGame(setRoomId);
+        }}
+        className="form-button center"
+      >
+        {DTO.GetRoomId}
+      </div>
+      <input
+        placeholder={DTO.RoomId}
+        className=""
+        defaultValue={roomId ? roomId : ""}
+      />
+      <div onClick={restart} className="form-button center">
+        {DTO.Back}
+      </div>
     </div>
   );
 };
