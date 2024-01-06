461110d60e03cff5babfaf5531775357f9d51eea
Roma <toftlegokl@gmail.com>
2024-01-06 22:46:00 +0200

    add OnlineTable translate

--- frontend/src/Data/Game/GameData.ts
+++ frontend/src/Data/Game/GameData.ts
@@ -8,6 +8,16 @@ type Table = {
     RestartGame: string;
 }
 
+type OnlineTable = {
+    Win: string;
+    Surendered: string;
+    Moving : string;
+    White: string;
+    Black: string;
+    GameHistory: string;
+    Surrender: string;
+}
+
 type GameData = {
     GetRoomId: string;
     RoomId: string;
@@ -15,6 +25,7 @@ type GameData = {
     CreateRoom: string;
     JoinRoom: string;
     Table: Table;
+    OnlineTable: OnlineTable;
 }
 
 export default GameData;
