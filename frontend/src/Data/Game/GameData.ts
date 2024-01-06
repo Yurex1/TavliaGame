c5292e16cdcaa017ac5b9cbb5083455503809ff4
Roma <toftlegokl@gmail.com>
2024-01-06 22:35:16 +0200

    add table translate

--- frontend/src/Data/Game/GameData.ts
+++ frontend/src/Data/Game/GameData.ts
@@ -1,9 +1,20 @@
+type Table = {
+    White: string;
+    Black: string;
+    Moveing: string;
+    WhiteWin: string;
+    BlackWin: string;
+    GameHistory: string;
+    RestartGame: string;
+}
+
 type GameData = {
     GetRoomId: string;
     RoomId: string;
     Back: string;
     CreateRoom: string;
     JoinRoom: string;
+    Table: Table;
 }
 
 export default GameData;
