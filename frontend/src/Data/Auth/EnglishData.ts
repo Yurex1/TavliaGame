15d3838d74dce147741043cfb70a035abc8a75d5
Roma <toftlegokl@gmail.com>
2024-01-07 20:50:06 +0200

    add translate error on auth

--- frontend/src/Data/Auth/EnglishData.ts
+++ frontend/src/Data/Auth/EnglishData.ts
@@ -9,4 +9,11 @@ export const EnglishData = {
   Name: "Login",
   AlreadyHaveAnAccount: "I already have an account",
   DontHaveAnAccount: "I don't have an account",
+  Problam: [
+    "Incorect username or password",
+    "All fields must be filled",
+    "Invalid email",
+    "Password must be at least 8 characters long",
+    "Email or login already exists"
+  ],
 } as AuthData;
